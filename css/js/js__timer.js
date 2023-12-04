22d2a7254e27b25e0e81cd51da488b10205b222d
Bahrom9791 <abdimuratovbahrom@gmail.com>
2023-12-04 22:15:26 +0500

    fix bug

--- css/js/js__timer.js
+++ css/js/js__timer.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
  
-var deadline = "December 11 2023 18:00:00 GMT+0500"; // for endless timer
+var deadline = "December 9 2023 18:00:00 GMT+0500"; // for endless timer
 initializeClock('countdown', deadline);
